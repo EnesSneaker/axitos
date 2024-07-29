1a023e641762aaf194b50aeb7d31e22311db9c55
Enes Ejubovic <enes.ejubovic.bus@gmail.com>
2024-07-29 19:45:44 +0200

    first commit

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div className="flex py-3 justify-between">
+      <div className="font-bold text-3xl pl-4 flex justify-center items-center">
+        AXITOS
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/section.tsx
@@ -0,0 +1,10 @@
+const Section = () => {
+  return (
+    <div className="w-full bg-black py-8 leading-none fixed bottom-0">
+      <p className="text-gray-400 pl-4">Design & </p>
+      <p className="text-white pl-4">Development</p>
+    </div>
+  );
+};
+
+export default Section;
